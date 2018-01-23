d1f275f290c936c4845189ecc469c74c5df2d653
Sultan Kenjeyev <cerber@Sultans-MacBook-Pro.local>
2018-01-23 12:53:14 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, Response
+from functools import wraps
+
+app = Flask(__name__)
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route('/')
+def home_route():
+    return "Hello world!"
+
+
+@app.route('/new_service')
+@requires_auth
+def register_service():
+    # adds to the automatic service checkup routine
+    return "registered"
